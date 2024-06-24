01d1969b57754811b5060cd353570e1453e7ec23
manhir <53478716+manhir@users.noreply.github.com>
2024-06-24 18:38:40 +0300

    simplify News

--- src/collections/News.ts
+++ src/collections/News.ts
@@ -1,6 +1,3 @@
-import { isAdmin } from '../access/isAdmin';
-import { isAdminOrHasSiteAccessOrPublished } from '../access/isAdminHasSiteAccessOrPublished';
-import { isAdminOrHasSiteAccess } from '../access/isAdminOrHasSiteAccess';
 import { isLoggedIn } from '../access/isLoggedIn';
 
 export const News = {
@@ -14,13 +11,6 @@ export const News = {
     access: {
         // Anyone logged in can create
         create: isLoggedIn,
-        // Only admins or editors with site access can update
-        update: isAdminOrHasSiteAccess(),
-        // Admins or editors with site access can read,
-        // otherwise users not logged in can only read published
-        read: isAdminOrHasSiteAccessOrPublished,
-        // Only admins can delete
-        delete: isAdmin,
     },
     fields: [
         {
