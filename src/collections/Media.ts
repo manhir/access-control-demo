6b8d98f5911abd015cc5ab383aa59d0907e7b674
manhir <53478716+manhir@users.noreply.github.com>
2024-06-25 12:48:39 +0300

    upd Sites and Media collections

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -4,36 +4,65 @@ import { isAdminOrHasSiteAccess } from "../access/isAdminOrHasSiteAccess";
 import { isLoggedIn } from "../access/isLoggedIn";
 
 export const Media: CollectionConfig = {
-  slug: 'media',
-  upload: true,
-  access: {
-    // Anyone logged in can create
-    create: isLoggedIn,
-    // Only admins or editors with site access can update
-    update: isAdminOrHasSiteAccess(),
-    // Only admins or editors with site access can read
-    read: isAdminOrHasSiteAccess(),
-    // Only admins or editors with site access can delete
-    delete: isAdminOrHasSiteAccess(),
-  },
-  fields: [
-    {
-      name: 'alt',
-      type: 'text',
-      required: true,
+    slug: 'media',
+    upload: {
+        // staticURL: '/media',
+        // staticDir: 'media',
+        imageSizes: [
+            {
+                name: 'thumbnail',
+                width: 400,
+                height: 300,
+                position: 'centre',
+            },
+            {
+                name: 'card',
+                width: 768,
+                height: 1024,
+                position: 'centre',
+            },
+            {
+                name: 'tablet',
+                width: 1024,
+                // By specifying `undefined` or leaving a height undefined,
+                // the image will be sized to a certain width,
+                // but it will retain its original aspect ratio
+                // and calculate a height automatically.
+                height: undefined,
+                position: 'centre',
+            },
+        ],
+        adminThumbnail: 'thumbnail',
+        mimeTypes: ['image/*'],
     },
-    {
-      name: 'site',
-      type: 'relationship',
-      relationTo: 'sites',
-      required: true,
-      // If user is not admin, set the site by default
-      // to the first site that they have access to
-      defaultValue: ({ user }) => {
-        if (!user.roles.includes('admin') && user.sites?.[0]) {
-          return user.sites[0];
+    access: {
+        // Anyone logged in can create
+        create: isLoggedIn,
+        // Only admins or editors with site access can update
+        update: isAdminOrHasSiteAccess(),
+        // Only admins or editors with site access can read
+        read: isAdminOrHasSiteAccess(),
+        // Only admins or editors with site access can delete
+        delete: isAdminOrHasSiteAccess(),
+    },
+    fields: [
+        {
+            name: 'alt',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'site',
+            type: 'relationship',
+            relationTo: 'sites',
+            required: true,
+            // If user is not admin, set the site by default
+            // to the first site that they have access to
+            defaultValue: ({ user }) => {
+                if (!user.roles.includes('admin') && user.sites?.[0]) {
+                    return user.sites[0];
+                }
+            }
         }
-      }
-    }
-  ]
+    ]
 }
--- src/collections/Sites.ts
+++ src/collections/Sites.ts
@@ -3,25 +3,29 @@ import { isAdmin } from "../access/isAdmin";
 import { isAdminOrHasSiteAccess } from "../access/isAdminOrHasSiteAccess";
 
 export const Sites: CollectionConfig = {
-  slug: 'sites',
-  admin: {
-    useAsTitle: 'title',
-  },
-  access: {
-    // Only admins can create
-    create: isAdmin,
-    // Only admins or editors with site access can read
-    read: isAdminOrHasSiteAccess('id'),
-    // Only admins can update
-    update: isAdmin,
-    // Only admins can delete
-    delete: isAdmin,
-  },
-  fields: [
-    {
-      name: 'title',
-      type: 'text',
-      required: true,
-    }
-  ]
+    slug: 'sites',
+    admin: {
+        useAsTitle: 'title',
+    },
+    access: {
+        // Only admins can create
+        create: isAdmin,
+        // Only admins or editors with site access can read
+        read: isAdminOrHasSiteAccess('id'),
+        // Only admins can update
+        update: isAdmin,
+        // Only admins can delete
+        delete: isAdmin,
+    },
+    fields: [
+        {
+            name: 'title',
+            type: 'text',
+            required: true,
+        },
+        {
+            name: 'description',
+            type: 'text',
+        }
+    ]
 }
