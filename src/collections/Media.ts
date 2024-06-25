28124b1338790d603aae637a3b5259e5e69fafe9
manhir <53478716+manhir@users.noreply.github.com>
2024-06-25 13:43:17 +0300

    upd Media upload to default config

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -5,36 +5,35 @@ import { isLoggedIn } from "../access/isLoggedIn";
 
 export const Media: CollectionConfig = {
     slug: 'media',
-    upload: {
-        // staticURL: '/media',
-        // staticDir: 'media',
-        imageSizes: [
-            {
-                name: 'thumbnail',
-                width: 400,
-                height: 300,
-                position: 'centre',
-            },
-            {
-                name: 'card',
-                width: 768,
-                height: 1024,
-                position: 'centre',
-            },
-            {
-                name: 'tablet',
-                width: 1024,
-                // By specifying `undefined` or leaving a height undefined,
-                // the image will be sized to a certain width,
-                // but it will retain its original aspect ratio
-                // and calculate a height automatically.
-                height: undefined,
-                position: 'centre',
-            },
-        ],
-        adminThumbnail: 'thumbnail',
-        mimeTypes: ['image/*'],
-    },
+    upload: true,
+    // upload: {
+    //     imageSizes: [
+    //         {
+    //             name: 'thumbnail',
+    //             width: 400,
+    //             height: 300,
+    //             position: 'centre',
+    //         },
+    //         {
+    //             name: 'card',
+    //             width: 768,
+    //             height: 1024,
+    //             position: 'centre',
+    //         },
+    //         {
+    //             name: 'tablet',
+    //             width: 1024,
+    //             // By specifying `undefined` or leaving a height undefined,
+    //             // the image will be sized to a certain width,
+    //             // but it will retain its original aspect ratio
+    //             // and calculate a height automatically.
+    //             height: undefined,
+    //             position: 'centre',
+    //         },
+    //     ],
+    //     adminThumbnail: 'thumbnail',
+    //     mimeTypes: ['image/*'],
+    // },
     access: {
         // Anyone logged in can create
         create: isLoggedIn,
